55f0d8e9439a8c0d5e22812e91a7f708b63591f0
Pham Hoai Nam <namph2102@gmail.com>
2023-11-09 23:43:56 +0700

    firsre update

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: "mongodb+srv://vocab:dt01626746032@cluster0.2ktatdl.mongodb.net/?retryWrites=true&w=majority",
+  options: {
+    user: "vocab",
+    pass: "dt01626746032",
+    dbName: "vocabs", // red
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //'autoIndex':       false,
+  },
+};
